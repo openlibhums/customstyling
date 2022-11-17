461b5917471be74331f9555ddfb2281ece4e66a6
Andy Byers <a.byers@bbk.ac.uk>
2022-11-17 12:31:45 +0000

    Plugin now runs on Django 3.2

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from plugins.customstyling import views
 
 
 urlpatterns = [
-    url(r'^manager/$', views.manager, name='customstyling_manager'),
-    url(r'^manager/settings/$', views.settings, name='customstyling_settings'),
-    url(r'^manager/press/$', views.manage_press_css, name='customstyling_manage_press_css'),
-    url(r'^manager/journal/(?P<journal_id>\d+)$', views.manage_css, name='customstyling_manage_css'),
+    re_path(r'^manager/$', views.manager, name='customstyling_manager'),
+    re_path(r'^manager/settings/$', views.settings, name='customstyling_settings'),
+    re_path(r'^manager/press/$', views.manage_press_css, name='customstyling_manage_press_css'),
+    re_path(r'^manager/journal/(?P<journal_id>\d+)$', views.manage_css, name='customstyling_manage_css'),
 
-    url(r'^manager/cjs/new/$', views.manage_cross_journal_stylesheets, name='customstyling_new_lcjsc'),
-    url(r'^manager/cjs/(?P<stylesheet_id>\d+)/$', views.manage_cross_journal_stylesheets, name='customstyling_edit_lcjsc'),
+    re_path(r'^manager/cjs/new/$', views.manage_cross_journal_stylesheets, name='customstyling_new_lcjsc'),
+    re_path(r'^manager/cjs/(?P<stylesheet_id>\d+)/$', views.manage_cross_journal_stylesheets, name='customstyling_edit_lcjsc'),
 ]
